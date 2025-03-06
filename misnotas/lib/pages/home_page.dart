2202846fbb405fab42a2b181cb519c0f1b3111b2
devarturobl <4rthurbl00@gmail.com>
2025-03-06 15:42:57 -0600

    Notes50

--- misnotas/lib/pages/home_page.dart
+++ misnotas/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:misnotas/services/firebase_options.dart';
 
 class HomePage extends StatefulWidget {
@@ -38,6 +39,36 @@ class _HomePageState extends State<HomePage> {
             ));
   }
 
+  void deleteNote() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: Text("¿Deseas Eliminar la Nota?"),
+              actions: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Si")),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("No")),
+              ],
+            ));
+  }
+
+  final List<Color> colores = [
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.purple,
+    Colors.pink,
+    Colors.brown,
+    Colors.deepOrange
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,17 +80,49 @@ class _HomePageState extends State<HomePage> {
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               List<DocumentSnapshot> noteList = snapshot.data!.docs;
-              return GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 15,
-                      mainAxisSpacing: 15,
-                      childAspectRatio: 1.2),
-                  itemCount: noteList.length,
-                  itemBuilder: (context, index) {
-                    DocumentSnapshot document = noteList[index];
-                    return Text(document['title']);
-                  });
+              return Padding(
+                padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 15,
+                        mainAxisSpacing: 15,
+                        childAspectRatio: 1.5),
+                    itemCount: noteList.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot document = noteList[index];
+                      return GestureDetector(
+                        onLongPress: () {
+                          deleteNote();
+                        },
+                        child: Container(
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                                color: colores[index % colores.length],
+                                borderRadius: BorderRadius.circular(20)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  document['title'],
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20),
+                                ),
+                                Text(
+                                  DateFormat('dd/MM/yyyy HH:mm').format(
+                                    (document['timestamp'] as Timestamp)
+                                        .toDate(),
+                                  ),
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                      color: Colors.white, fontSize: 15),
+                                )
+                              ],
+                            )),
+                      );
+                    }),
+              );
             } else {
               return CircularProgressIndicator();
             }
